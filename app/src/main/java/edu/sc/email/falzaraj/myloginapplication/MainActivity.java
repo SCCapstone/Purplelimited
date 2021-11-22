38b69c064a05447969aaf03681323bf49f1a9bb4
jmfalzarano <48529876+jmfalzarano@users.noreply.github.com>
2021-11-22 14:36:50 -0500

    Added security feature and user classes
    Made a login max number of allowable attempts to login as well as a super basic class we can expand upon to save users data

--- app/src/main/java/edu/sc/email/falzaraj/myloginapplication/MainActivity.java
+++ app/src/main/java/edu/sc/email/falzaraj/myloginapplication/MainActivity.java
@@ -7,9 +7,9 @@ import android.content.Intent;
 import android.os.Bundle;
 import android.view.View;
 import android.widget.Button;
+import android.widget.EditText;
 import android.widget.TextView;
 import android.widget.Toast;
-
 import com.google.android.gms.auth.api.Auth;
 import com.google.android.gms.auth.api.signin.GoogleSignInOptions;
 import com.google.android.gms.common.ConnectionResult;
@@ -20,31 +20,83 @@ import com.google.android.material.button.MaterialButton;
 
 public class MainActivity extends AppCompatActivity{
 
-    SignInButton signInButton;
-    private static final int SIGN_IN = 1;
+    private EditText Name;
+    private EditText Password;
+    private TextView AttemptsInfo;
+    private Button Login;
+    private int cnt = 3; //base amount of login attempts
 
-    @Override //doing a googleAPISign in with deprecated classes
+    String userName = "admin";
+    String userPassword = "admin"; //these are the default hard-coded strings for a user account for the interim
+
+   //class for user account information
+    class UserAccount
+    {
+        TextView Name = findViewById(R.id.etName);
+        TextView Password = findViewById(R.id.etPassword);
+    }
+
+    boolean isValid = false;
+
+    @Override
     protected void onCreate(Bundle savedInstanceState) {
         super.onCreate(savedInstanceState);
         setContentView(R.layout.activity_main);
 
-        TextView username1 = findViewById(R.id.username1);
-        TextView password = findViewById(R.id.password);
+        // Binding the XML views
+        Name = findViewById(R.id.etName);
+        Password = findViewById(R.id.etPassword);
+        AttemptsInfo = findViewById(R.id.noAttempts);
+        Login = findViewById(R.id.loginbutton);
 
-        MaterialButton loginbutton = (MaterialButton)  findViewById(R.id.loginbutton);
-
-        //admin & admin example
-
-        loginbutton.setOnClickListener(new View.OnClickListener() {
+        // When login button is clicked
+            Login.setOnClickListener(new View.OnClickListener() {
             @Override
-            public void onClick(View v) {
-                if (username1.getText().toString().equals("admin") && password.getText().toString().equals("admin")){
-                    //correct login
-                    Toast.makeText(MainActivity.this, "LOGIN SUCCESSFUL", Toast.LENGTH_SHORT).show();
-                }else
-                    //incorrect login
-                    Toast.makeText(MainActivity.this, "LOGIN UNSUCCESSFUL", Toast.LENGTH_SHORT).show();
+            public void onClick(View view) {
+
+                // user inputs
+                userName = Name.getText().toString();
+                userPassword = Password.getText().toString();
+
+                if(userName.isEmpty() || userPassword.isEmpty()) {
+                    Toast.makeText(MainActivity.this, "Please enter a username and password!", Toast.LENGTH_LONG).show();
+                }
+                else {
+
+                    // Validate the user inputs
+                    isValid = validate(userName, userPassword);
+                    if (!isValid) {
+                        cnt--;
+                        AttemptsInfo.setText("Attempts Remaining: " + String.valueOf(cnt));
+                        // Disable login button when counter == 0
+                        if (cnt == 0) {
+                            Login.setEnabled(false);
+                            Toast.makeText(MainActivity.this, "You have used all your attempts", Toast.LENGTH_LONG).show();
+                        }
+                        else {
+                            Toast.makeText(MainActivity.this, "Incorrect credentials, please try again!", Toast.LENGTH_LONG).show();
+                        }
+                    }
+                    else {
+                        Toast.makeText(MainActivity.this, "Login Successful!", Toast.LENGTH_SHORT).show();
+                        // Send user to the next activity *DO NOT USE THIS YET, this if for transitioning once the files are merged
+                        // startActivity(new Intent(MainActivity.this, HomePageActivity.class));
+                    }
+
+                }
             }
         });
+    }
+
+    private boolean validate(String userName, String userPassword)
+    {
+        // Get the object of UserAccount class
+        UserAccount userAccount = new UserAccount();
+
+        // Match user details
+        if(userName.equals(userAccount.Name) && userPassword.equals(userAccount.Password)) {
+            return true;
+        }
+        return false;
     }
 }
