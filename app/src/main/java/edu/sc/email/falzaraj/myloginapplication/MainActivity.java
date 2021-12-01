44efafae46a5f935f7bcd232b628cb3e7f20ba03
ABuonantuono <89362652+ABuonantuono@users.noreply.github.com>
2021-12-01 17:27:42 -0500

    Classes/drawables

--- app/src/main/java/edu/sc/email/falzaraj/myloginapplication/MainActivity.java
+++ app/src/main/java/edu/sc/email/falzaraj/myloginapplication/MainActivity.java
@@ -19,6 +19,36 @@ import java.util.ArrayList;
 
 public class MainActivity extends AppCompatActivity{
 
+
+    private void loadCards() {
+        recipes = new ArrayList<>();
+
+        recipes.add(new Model(
+                "Beef and Broccoli",
+                "Tender strips of steak and crisp broccoli florets in a rich ginger and garlic sauce",
+                R.drawable.beefnbroc));
+        recipes.add(new Model(
+                "Meatball Sub",
+                "Tender juicy meatballs simmered in a flavorful tomato sauce are placed in a roll and topped with cheese",
+                R.drawable.meatballsub));
+        recipes.add(new Model(
+                "Penne Vodka",
+                "Made with heavy cream, crushed tomatoes, onions",
+                R.drawable.pennevodka));
+        recipes.add(new Model(
+                "Stuffed Red Peppers",
+                "Hollowed or halved peppers filled with any of a variety of fillings, often including meat, vegetables, cheese, rice, or sauce",
+                R.drawable.stuffedrpeppers));
+
+        adapter = new Adapter(this, recipes);
+        viewPager.setAdapter(adapter);
+        viewPager.setPadding(100, 0,100,0);
+
+    }
+
+
+
+
     // template class
     public class DataBaseReader {
         // stream of data from data base
@@ -76,55 +106,6 @@ public class MainActivity extends AppCompatActivity{
                     Toast.makeText(MainActivity.this, "LOGIN UNSUCCESSFUL", Toast.LENGTH_SHORT).show();
             }
         });
-    }
-
-
-    private ActionBar actionbar;
-    private ViewPager viewPager;
-    private ArrayList<Model> modelArrayList;
-    private Adapter adapter;
-    @Override
-
-    protected void onCreate(Bundle savedInstanceState) {
-        super.onCreate(savedInstanceState);
-        setContentView(R.layout.activity_main);
-
-
-        actionbar = getSupportActionBar();
-
-        viewPager = findViewById(R.id.viewPager);
-        loadCards();
-
-        viewPager.addOnPageChangeListener(new ViewPager.OnPageChangeListener() {
-            @Override
-            public void onPageScrolled(int position, float positionOffset, int positionOffsetPixels) {
-                String title = modelArrayList.get(position).getTitle();
-                actionbar.setTitle(title);
-            }
-
-            @Override
-            public void onPageSelected(int position) {
-
-            }
-
-            @Override
-            public void onPageScrollStateChanged(int state) {
-
-            }
-        });
-
-    }
-
-    private void loadCards() {
-        modelArrayList = new ArrayList<Model>();
-        modelArrayList.add(new Model("Beef and Broccoli", "Tender strips of steak and crisp broccoli florets in a rich ginger and garlic sauce.", R.drawable.beefbroc));
-        modelArrayList.add(new Model("Meatball Sub", "Tender juicy meatballs simmered in a flavorful tomato sauce are placed in a roll and topped with cheese.", R.drawable.meatballsub));
-        modelArrayList.add(new Model("Penne Vodka", "Made with heavy cream, crushed tomatoes, onions", R.drawable.pennevodka));
-        modelArrayList.add(new Model("Stuffed Red Peppers", "Hollowed or halved peppers filled with any of a variety of fillings, often including meat, vegetables, cheese, rice, or sauce", R.drawable.stuffedrpeppers));
-
-        adapter = new Adapter(this, modelArrayList);
-        viewPager.setAdapter(Adapter);
-        viewPager.setPadding(100, 0, 100, 0);
     }
 
 
