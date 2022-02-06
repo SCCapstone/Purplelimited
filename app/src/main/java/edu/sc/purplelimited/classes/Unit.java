a83d198c3778131d236da05257d181760a27c1d4
coy <coy.dr.burke@gmail.com>
2022-02-06 11:14:06 -0500

    Added ingredient class and enum for units. Fixes #42

---
+++ app/src/main/java/edu/sc/purplelimited/classes/Unit.java
@@ -0,0 +1,15 @@
+package edu.sc.purplelimited.classes;
+
+public enum Unit {
+  CUP,
+  FLUID_OUNCE,
+  GALLON,
+  GRAM,
+  LITER,
+  MILLILITER,
+  OUNCE,
+  PINT,
+  QUART,
+  TABLESPOON,
+  TEASPOON,
+}
