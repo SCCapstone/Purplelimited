3d1f40886876aea4606fc34dae27db6825ba84c6
jmfalzarano <48529876+jmfalzarano@users.noreply.github.com>
2022-03-19 13:42:16 -0400

    Logout Button
    Addition of a logout button in the main activity, located in the top right hand corner of the screen. IF/ELSE loop in main was switched to a while loop so the logout button may kick the user out of the logged in loop

--- app/src/main/java/edu/sc/purplelimited/MainActivity.java
+++ app/src/main/java/edu/sc/purplelimited/MainActivity.java
@@ -1,9 +1,7 @@
 package edu.sc.purplelimited;
 
-import android.content.SharedPreferences;
-import android.os.Bundle;
-
 import android.content.Intent;
+import android.os.Bundle;
 import android.view.View;
 import android.widget.Button;
 
@@ -13,16 +11,13 @@ import androidx.navigation.Navigation;
 import androidx.navigation.ui.AppBarConfiguration;
 import androidx.navigation.ui.NavigationUI;
 
-
-import java.util.ArrayList;
-
-import edu.sc.purplelimited.R;
 import edu.sc.purplelimited.databinding.ActivityMainBinding;
 
 public class MainActivity extends AppCompatActivity {
 
     private static boolean loggedIn = true;
     private static int userId = 000;
+    private Button Logout;
 
 //    private Button save_recipe_button;
 //    public static final String SHARED_PREFS = "sharedPrefs";
@@ -31,10 +26,13 @@ public class MainActivity extends AppCompatActivity {
     @Override
     protected void onCreate(Bundle savedInstanceState) {
         super.onCreate(savedInstanceState);
+        ActivityMainBinding binding;
+        binding = ActivityMainBinding.inflate(getLayoutInflater());
 
+        // Binding the XML views
+        Logout = findViewById(R.id.logoutbutton);
 
-        ActivityMainBinding binding = ActivityMainBinding.inflate(getLayoutInflater());
-        if(loggedIn) {                                  //TODO this might be better as a while loop
+        while(loggedIn = true) {                                  //changed to a while loop
             setContentView(binding.getRoot());
             findViewById(R.id.nav_view);
             AppBarConfiguration appBarConfiguration = new AppBarConfiguration.Builder(
@@ -43,11 +41,16 @@ public class MainActivity extends AppCompatActivity {
             NavController navController = Navigation.findNavController(this, R.id.nav_host_fragment_activity_main);
             NavigationUI.setupActionBarWithNavController(this, navController, appBarConfiguration);
             NavigationUI.setupWithNavController(binding.navView, navController);
+
+            Logout.setOnClickListener(new View.OnClickListener() {  //logout button forces while loop to end
+                @Override
+                public void onClick(View view) {
+                    loggedIn = false;
+                }
+            });
         }
-        else {
-            //Takes user back to login page when loggedIn = false
+        //Takes user back to login page when loggedIn = false
         startActivity(new Intent(MainActivity.this, LoginActivity.class));
-        }
     }
 
     public static void setLoggedIn(Boolean bool) {
