0f9d865ecc1bee67c1ef43024030ca789f2aa58b
Jackson K Puente <jpuente@email.sc.edu>
2022-04-04 00:01:41 -0400

    RegistrationActivity Testing

---
+++ app/src/androidTest/java/edu/sc/purplelimited/RegistrationActivityInstrumentedTest.java
@@ -0,0 +1,29 @@
+package edu.sc.purplelimited;
+
+import static androidx.test.espresso.Espresso.onView;
+import static androidx.test.espresso.assertion.ViewAssertions.matches;
+import static androidx.test.espresso.matcher.ViewMatchers.isDisplayed;
+import static androidx.test.espresso.matcher.ViewMatchers.withText;
+
+import androidx.test.core.app.ActivityScenario;
+import androidx.test.ext.junit.rules.ActivityScenarioRule;
+import androidx.test.ext.junit.runners.AndroidJUnit4;
+import androidx.test.filters.LargeTest;
+
+import org.junit.Rule;
+import org.junit.Test;
+import org.junit.runner.RunWith;
+
+@RunWith(AndroidJUnit4.class)
+@LargeTest
+public class RegistrationActivityInstrumentedTest {
+
+    @Rule
+    public ActivityScenarioRule<RegistrationActivity> activityRule =
+            new ActivityScenarioRule<>(RegistrationActivity.class);
+
+    @Test
+    public void signInTextIsDisplayed() {
+        onView(withText("Sign in")).check(matches(isDisplayed()));
+    }
+}
